d08d98e20c3c2483cc2d29b663e37384a851810c
Reza Azad <TestProjectJenkins@gmail.com>
2016-06-19 10:24:48 +0200

    some comments

--- Linux/ProgressState.h
+++ Linux/ProgressState.h
@@ -1,10 +1,18 @@
 #pragma once
 
+/** comments on bla bla
+ ....
+ ....
+*/
+
 class ProgressState {
 public:
   ProgressState(unsigned int target);
   virtual ~ProgressState();
- 
+ /**
+   bla bla
+   bla bla ....
+  */
 
   unsigned int getPercentage();
   
